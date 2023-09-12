17a820bd3a7f837e66e1a5886c18ba7f02155537
Philip Zimmermann <pzi@ito-consult.de>
2023-09-12 10:07:06 +0200

    create callbacks to read and write data

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const fs = require("fs");
 
 fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
-  console.log(data1);
+  fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./starter/txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile(
+        "./starter/txt/final-test.txt",
+        `${data2}\n${data3}`,
+        "utf-8",
+        (err) => {
+          console.log("Your file has been written!😀");
+        }
+      );
+    });
+  });
 });
 console.log("Will read file!");
