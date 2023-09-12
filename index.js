c48a8bf93caaa6ab6631d6a3c48b2dbafb514104
Philip Zimmermann <pzi@ito-consult.de>
2023-09-12 10:17:58 +0200

    create server and listen to it

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const fs = require("fs");
+const http = require("http");
 
-fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./starter/txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile(
-        "./starter/txt/final-test.txt",
-        `${data2}\n${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("Your file has been written!😀");
-        }
-      );
-    });
-  });
+///////////////////////////////////////////////////////
+// FILES
+
+// fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./starter/txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(
+//         "./starter/txt/final-test.txt",
+//         `${data2}\n${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("Your file has been written!😀");
+//         }
+//       );
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+///////////////////////////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
